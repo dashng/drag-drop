6ba57ce1c799ef29e7163e88f88af6da78a10234
shdai <shdai@cisco.com>
2017-01-08 17:15:01 +0800

    nb!

--- example/app.js
+++ example/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     var fb = new form_builder.create();
-    fb.add_elements({
+    fb.add_elements([{
         'btn': '<button>下拉框</button>',
         'field': '<select></select>'
-    });
+    },
+    {
+        'btn': '<button>input</button>',
+        'field': '<input placeholder="inset something here..." />'
+    }]);
     fb.add_forms('.form');
     fb.add_elements_box('.elements');
     fb.bootstrap();
